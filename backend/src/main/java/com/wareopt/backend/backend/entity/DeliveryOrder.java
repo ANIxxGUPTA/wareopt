f32a051a104b96a6cbb08615b0eb04e00284ebef
Ani Gupta <anisha.alumni@gmail.com>
2026-07-22 17:55:31 +0530

    Allow nullable columns to fix db schema update

--- backend/src/main/java/com/wareopt/backend/backend/entity/DeliveryOrder.java
+++ backend/src/main/java/com/wareopt/backend/backend/entity/DeliveryOrder.java
@@ -36,11 +36,11 @@ public class DeliveryOrder {
     private Integer priority;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false, columnDefinition = "varchar(255) default 'PENDING'")
+    @Column(nullable = true, columnDefinition = "varchar(255) default 'PENDING'")
     private OrderStatus status = OrderStatus.PENDING;
 
     @Version
-    @Column(nullable = false, columnDefinition = "bigint default 0")
+    @Column(nullable = true, columnDefinition = "bigint default 0")
     private Long version = 0L;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
